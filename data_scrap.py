3b4e6fa03279d57b54df72ad75bf154ef68d2731
jinyoung Lee <prin46@naver.com>
2026-04-01 13:47:23 +0900

    sqlite DB 연결

--- data_scrap.py
+++ data_scrap.py
@@ -1,5 +1,35 @@
 import requests
 import time
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('trickcal_cafe.db') 
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS articles (
+            article_id INTEGER PRIMARY KEY,
+            author TEXT,
+            title TEXT,
+            url TEXT,
+            is_collected INTEGER DEFAULT 0  -- 0: 미수집, 1: 완료
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def save_articles(article_list):
+    conn = sqlite3.connect('trickcal_cafe.db')
+    cursor = conn.cursor()
+    
+    for item in article_list:
+        # INSERT OR IGNORE를 쓰면 중복된 ID는 알아서 패스합니다.
+        cursor.execute('''
+            INSERT OR IGNORE INTO articles (article_id, author, title, url)
+            VALUES (?, ?, ?, ?)
+        ''', (item['id'], item['nickName'], item['title'], item['url']))
+        
+    conn.commit()
+    conn.close()
 
 def collect_article_urls(cafe_id, menu_id, page_limit=3):
     base_api_url = f"https://apis.naver.com/cafe-web/cafe-boardlist-api/v1/cafes/{cafe_id}/menus/{menu_id}/articles"
@@ -51,9 +81,9 @@ def collect_article_urls(cafe_id, menu_id, page_limit=3):
             
     return all_articles
 
-cafe_id = "30131231"
-menu_id = "22"
-collected_data = collect_article_urls(cafe_id, menu_id, page_limit=1)
-
-for data in collected_data :
-    print(data['nickName'], data['title'], data['url'])
+if __name__ == "__main__" :
+    init_db()
+    cafe_id = "30131231"
+    menu_id = "22"
+    article_list = collect_article_urls(cafe_id, menu_id, page_limit=1)
+    save_articles(article_list)
